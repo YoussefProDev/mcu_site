d574a469d407e2e30f4277eea9a878247ada1087
YoussefProDev <youssef23468210@gmail.com>
2023-04-29 20:50:12 +0200

    creazione di file statici e modifiche al gitignore

--- mcu_site/urls.py
+++ mcu_site/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 # from home import urls
 
 urlpatterns = [
@@ -23,3 +25,6 @@ urlpatterns = [
     path('',include('home.urls')),
     path('personaggi/',include('personaggi.urls')),
 ]
+# ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
