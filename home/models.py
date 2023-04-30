da6f277692713da099c08d5531b34fa8b65e759e
YoussefProDev <youssef23468210@gmail.com>
2023-04-30 13:22:37 +0200

    *cambiato registrazone admin personaggi *messo filtri nel admin *aggiunta nel models personaggi img, descrizione, slug e abilita *nel model aggiunta def __str__ *eliminato field nel modello home *nel modello home aggiunta foreignkey di personaggi

--- home/models.py
+++ home/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+# from django.conf import settings
+from personaggi.models import Personaggi
 
 # Create your models here.
 
 class Home(models.Model):
-    nome = models.TextField(max_length=50)
-    sopranome = models.TextField(max_length=50)
+    # nome = models.TextField(max_length=50)
+    # sopranome = models.TextField(max_length=50)
+    personaggi = models.ForeignKey(Personaggi,on_delete= models.CASCADE, null=True)
     
--- personaggi/admin.py
+++ personaggi/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Personaggi
 # Register your models here.
-admin.site.register(Personaggi)
+# admin.site.register(Personaggi)
+
+
+@admin.register(Personaggi)
+class PersonaggiAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('nome',)}
+    list_display = ('nome','sopranome', 'attore')
+    search_fields = ('nome','sopranome', 'attore')
+    ordering = ("nome",)
+
