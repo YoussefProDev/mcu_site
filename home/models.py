1e6cf3f446f14fa28f92dc6d7b71e0da4630a652
YoussefProDev <youssef23468210@gmail.com>
2023-04-30 08:57:18 +0200

    aggiustamento gitignore

--- home/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Home(models.Model):
-    nome = models.TextField(max_length=50)
-    sopranome = models.TextField(max_length=50)
-    
--- personaggi/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Personaggi(models.Model):
-    nome = models.TextField(max_length=50)
-    sopranome = models.TextField(max_length=50)
-    attore = models.TextField(max_length=50,null=True)
