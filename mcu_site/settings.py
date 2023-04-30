d1e2131964cc4d6c0759409cd43a8bfda3180129
YoussefProDev <youssef23468210@gmail.com>
2023-04-30 18:37:47 +0200

    *funziona tailwindcss

--- mcu_site/settings.py
+++ mcu_site/settings.py
@@ -119,12 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'static/')
+# STATIC_ROOT = Path.joinpath(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'productionfiles'
-# STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static/')
-# ]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR , 'static/')
+]
 
 
 # Default primary key field type
--- mcu_site/urls.py
+++ mcu_site/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
 ]
 # ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
