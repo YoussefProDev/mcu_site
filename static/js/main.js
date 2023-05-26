b7a5b4b61253bd292ec5530708ea6a21bf5dd60e
YoussefProDev <youssef23468210@gmail.com>
2023-05-26 11:14:59 +0200

    *aggiustato la nav

--- static/js/main.js
+++ static/js/main.js
@@ -7,14 +7,22 @@ let nextButton = document.getElementsByClassName("next");
 console.log(nextButton);
 let prevButton = document.getElementsByClassName("prev");
 console.log(prevButton);
-console.log("hello world"); 
-console.log("fuziona");
-console.log(x)
-console.log(slider.offsetWidth );
-console.log(window.innerWidth);
+// console.log("hello world"); 
+// console.log("fuziona");
+// console.log(x)
+// console.log(slider.offsetWidth );
+// console.log(window.innerWidth);
 // console.log(nextButton.);
 nextButton.onclick = translateToRight   
 prevButton.onclick = translateToLeft
+const btn = document.querySelector("button.menu-button");
+const menu = document.querySelector(".menu");
+
+// Add Event Listeners
+btn.addEventListener("click", () => {
+menu.classList.toggle("hidden");
+menu.classList.toggle('open-menu')
+});
 for (let i = 0; i < nextButton.length; i++) {
     nextButton[i].addEventListener('click', translateToRight) 
     prevButton[i].addEventListener('click', translateToLeft) 
