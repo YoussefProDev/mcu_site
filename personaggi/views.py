2f6473b5c3d3d4996f97b5920170dc61745e5b45
YoussefProDev <youssef23468210@gmail.com>
2023-05-26 11:43:44 +0200

    *cancellato i file siperflui

--- personaggi/views.py
+++ personaggi/views.py
@@ -7,22 +7,6 @@ from django.views.generic.list import ListView
 from productions.models import Productions
 
 
-# Create your views here.
-# def PersonaggiView(request):
-
-#     obj = Personaggi.objects.all()
-#     context = {}
-#     context['attore'] = obj[0].attore
-#     return render(request,'personaggi/index.html',  context=context)
-
-class PersonaggiView(TemplateView):
-    template_name = 'personaggi/index.html'
-
-    def get_context_data(self, **kwargs):
-
-        context = super().get_context_data(**kwargs)
-        context['attore'] = Personaggi.objects.all()[1].attore
-        return context
 
 class PersonaggiDetail(DetailView):
     model = Personaggi
--- productions/views.py
+++ productions/views.py
@@ -9,23 +9,6 @@ from .forms import FilterForm
 from django.db.models import Q
 
 
-# Create your views here.
-# def PersonaggiView(request):
-
-#     obj = Personaggi.objects.all()
-#     context = {}
-#     context['attore'] = obj[0].attore
-#     return render(request,'personaggi/index.html',  context=context)
-
-class ProductionsView(TemplateView):
-    template_name = 'personaggi/index.html'
-
-    def get_context_data(self, **kwargs):
-
-        context = super().get_context_data(**kwargs)
-        # context['attore'] = Personaggi.objects.all()[1].attore
-        return context
-
 class ProductionsDetail(DetailView):
     model = Productions
     template_name = "productions/productions_detail.html"
