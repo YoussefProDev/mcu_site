7acf404758e40c300e5f04cf08b6a78bf0ab2194
YoussefProDev <youssef23468210@gmail.com>
2025-05-19 08:43:33 +0200

    test deploy 8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput 
 exec gunicorn mcu_site.wsgi:application --bind 0.0.0.0:8000
--- mcu_site/settings.py
+++ mcu_site/settings.py
@@ -128,12 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-# Static
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -145,6 +144,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
 
 
 # Default primary key field type
